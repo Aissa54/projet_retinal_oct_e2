7950ff880fd2fd1e9eb7092e835b0a58ef8b7505
Aissa54 <95747202+Aissa54@users.noreply.github.com>
2023-01-27 15:34:25 +0100

    api et interface à jour

--- app.py
+++ app.py
@@ -1,76 +1,47 @@
 import io
+from flask import Flask, request, render_template
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from flask import Flask, jsonify, request, render_template, Response
-import json
-from matplotlib.figure import Figure
-from io import BytesIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as VirtualCanvas
+import logging
 
+app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+logging.basicConfig(level=logging.DEBUG)
 model = tf.keras.models.load_model('model4_retinal-oct.h5')
 
 def prepare_image(img):
-    img = Image.open(io.BytesIO(img))
+    """
+    prepares the image for the api call
+    """
+    img = Image.open(io.BytesIO(img)).convert('RGB')
     img = img.resize((150, 150))
+    img = np.array(img)
     img = np.expand_dims(img, 0)
-    img = np.stack((img,)*3, axis=-1)
-    
     return img
 
-
 def predict_result(img):
-    Y_pred = model.predict(img)
-    print('Prédiction du modèle: ', Y_pred)
-    return np.argmax(Y_pred, axis=1)
-#argmax permet de trouver la valeur maximale donc la meilleure prédiction possible de la bonne pathologie
+    """predicts the result"""
+    return np.argmax(model.predict(img)[0])
 
-app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def infer_image():
     if 'file' not in request.files:
-        return "Veuillez réessayer. L'image n'existe pas"
-    
+        return "Please try again. The Image doesn't exist"
     file = request.files.get('file')
-
     if not file:
         return
-
     img_bytes = file.read()
     img = prepare_image(img_bytes)
-    return jsonify(prediction=int(predict_result(img)))
-    
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
-
-@app.route('/chart', methods=['GET'])
-def chart():
-    param = json.loads(request.args.get("param"))
-    print(param)
-    
-    labels = []
-    values =[]
-    
-    for key in param["datas"]:
-        labels.append(key)
-        values.append(param["datas"][key])
-    #print(labels)
-    #print(values)
-    
-    fig = Figure()
-    ax1 = fig.subplots(1, 1)
-    ax1.bar(labels, values)
-    #fig.savefig("test.png", format="png") 
-    
-    output = BytesIO()
-    VirtualCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
-
+    prediction = predict_result(img)
+    return render_template('predict.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', use_reloader=False)
