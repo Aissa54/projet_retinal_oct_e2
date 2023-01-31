2a50212b6149a1c78a4d7587e2313498f561609d
Aissa54 <95747202+Aissa54@users.noreply.github.com>
2023-01-31 10:30:02 +0100

    Mise à jour de la page predict.html

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 logging.basicConfig(level=logging.DEBUG)
 model = tf.keras.models.load_model('model4_retinal-oct.h5')
 
+classes = ['CNV', 'DME', 'DRUSEN', 'NORMAL']
+
 def prepare_image(img):
     """
     prepares the image for the api call
@@ -40,8 +42,16 @@ def infer_image():
         return
     img_bytes = file.read()
     img = prepare_image(img_bytes)
-    prediction = predict_result(img)
-    return render_template('predict.html', prediction=prediction)
+    predictions = model.predict(img)[0].tolist()
+    prediction = np.argmax(predictions)
+    
+    # Mapping prediction to class
+    class_mapping = {0: 'CNV', 1: 'DME', 2: 'DRUSEN', 3: 'NORMAL'}
+    prediction_class = class_mapping.get(prediction, "Unknown")
+    
+    return render_template('predict.html', prediction=prediction_class, class_probabilities=predictions)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', use_reloader=False)
